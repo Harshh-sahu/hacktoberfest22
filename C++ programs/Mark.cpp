61535b32e18e85eea4a3bca14481b7d5d549d9a5
Harsh sahu <116263893+harshsahu1-new@users.noreply.github.com>
2022-10-20 19:45:53 +0530

    Newfile.cpp

--- C++ programs/Mark.cpp
+++ C++ programs/Mark.cpp
@@ -3,25 +3,25 @@
 int main()
 {
 int sub1,sub2,sub3,sub4,sub5;
-float avg;
+float average;
 printf("Enter marks of 5 subjects:");
 scanf("%d%d%d%d%d",&sub1, &sub2, &sub3 ,&sub4 ,&sub5);
 avg=(sub1+sub2+sub3+sub4+sub5)/5;
-printf("Percentage = %f",avg);
-if(avg>=60)
+printf("Percentage = %f",average);
+if(average>=60)
 {
    printf("\nFirst Division");
 }
-else if(avg>=50)
+else if(average>=50)
 {
-   if(avg <=59)
+   if(average <=59)
    {
       printf("\nSecond Division");
    }
 }
-else if(avg >=40)
+else if(average >=40)
 {
-   if(avg <=49)
+   if(average <=49)
    {
        printf("\nThird Division");
    }
